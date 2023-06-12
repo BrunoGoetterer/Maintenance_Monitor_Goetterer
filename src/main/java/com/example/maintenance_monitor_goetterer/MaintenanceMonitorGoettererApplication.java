b938742db9e69f8ed22f89c2ee45397666f60635
BrunoGoetterer <wi22b058@technikum-wien.at>
2023-06-12 18:42:50 +0200

    Initial commit

---
+++ src/main/java/com/example/maintenance_monitor_goetterer/MaintenanceMonitorGoettererApplication.java
@@ -0,0 +1,13 @@
+package com.example.maintenance_monitor_goetterer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenanceMonitorGoettererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaintenanceMonitorGoettererApplication.class, args);
+	}
+
+}
